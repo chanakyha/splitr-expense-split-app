ea74d64d70f66d83750a480d2fe02d71ac1ae299
chanakyha <vetrichanakyha2003@gmail.com>
2023-08-29 00:28:42 +0530

    Routes Protected using serverSideActions

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,27 @@
 import Image from "next/image";
 import Logo from "@/public/images/logo.png";
 import { MenuIcon } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuRadioGroup,
+  DropdownMenuLabel,
+  DropdownMenuItem,
+} from "./ui/dropdown-menu";
+import { Switch } from "./ui/switch";
+import { signOut } from "next-auth/react";
+import { useTheme } from "next-themes";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const { setTheme } = useTheme();
+  const [themeChecked, setThemeChecked] = useState("light");
+  const { data: session } = useSession();
+  const router = useRouter();
   return (
     <div className="p-2 flex border-b items-center justify-between">
       <Image
@@ -13,7 +32,58 @@ const Header = () => {
         src={Logo}
       />
       <h1 className="font-medium text-lg uppercase tracking-widest">Splitr</h1>
-      <MenuIcon className="w-8 h-8" />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <MenuIcon className="w-8 h-8 cursor-pointer" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 mr-3">
+          <DropdownMenuLabel>
+            Hello {session ? session?.user?.name : "User"}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup value={"bottom"}>
+            {session ? (
+              <>
+                <DropdownMenuItem>Your Profile</DropdownMenuItem>
+                <DropdownMenuItem>View Trips</DropdownMenuItem>
+                <DropdownMenuItem>Due Pending</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="text-red-800 font-medium cursor-pointer">
+                  Logout
+                </DropdownMenuItem>
+              </>
+            ) : (
+              <DropdownMenuItem
+                onClick={() => {
+                  signOut();
+                  router.push("/login");
+                }}
+              >
+                Login
+              </DropdownMenuItem>
+            )}
+
+            <DropdownMenuSeparator />
+            <div className="flex text-sm capitalize items-center px-2 py-1.5 w-full justify-between">
+              <p className="font-medium">
+                Theme: <span className="font-light">{themeChecked}</span>
+              </p>
+              <Switch
+                checked={themeChecked === "dark"}
+                onCheckedChange={(checked) => {
+                  if (checked) {
+                    setTheme("dark");
+                    setThemeChecked("dark");
+                  } else {
+                    setTheme("light");
+                    setThemeChecked("light");
+                  }
+                }}
+              />
+            </div>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
+import { getServerSession } from "next-auth";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 const Login = () => {
   const { data: session } = useSession();
@@ -14,3 +16,22 @@ const Login = () => {
 };
 
 export default Login;
+
+export async function getServerSideProps(context: any) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (session?.user?.name) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
